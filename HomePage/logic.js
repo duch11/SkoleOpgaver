6638080d18a238f8768a9b75b8cf725a9cacfbe3
Jonas Ladefoged Holm <jonas@holm.tech>
2017-06-05 05:27:10 +0200

    Save

--- HomePage/logic.js
+++ HomePage/logic.js
@@ -1,45 +1,27 @@
 onload = init;
 
 function init(){
-    var fPageButton = document.getElementById("frontpage");
-    var resumeButton = document.getElementById("resume");
-    var projectsButton = document.getElementById("sparetime");
-    var dreamFirmButton = document.getElementById("dream");
+    displayFrontPage(); 
+    underConstruction();
+    var content = document.getElementById("content");
 
-    var heading = document.getElementById("title");
-    var ptag = document.getElementById("ptag");
-    var divTag = document.getElementById("content")
-
-    displayFrontPage();
-    displayResumePage();
-
-    fPageButton.onclick = displayFrontPage;
-    resumeButton.onclick = displayResumePage;
-    projectsButton.onclick = displayProjectsPage;
-    dreamFirmButton.onclick = displayDreamFirmPage;
+    frontpage.onclick = displayFrontPage;
+    resume.onclick = displayResumePage;
+    sparetime.onclick = displayProjectsPage;
+    dream.onclick = displayDreamFirmPage;
+    blog.onclick = displayBlog;
 }
 
 function displayFrontPage(){
     clearPage();
     heading.innerHTML = "Jonas Ladefoged Holm";
-    ptag.innerHTML = "This website is mostly a sparetime project, so don't expect marvolous things. However I'm making it from the bottom up. No cheating, only my code and some frameworks maybe.. (haven't decided yet) :) <br>If you are an employer, please look at my projects page to look what I've done in the past. Otherwise, you can read my resume at the provided menu option.";
+    ptag.innerHTML = "This website is mostly a sparetime project, so don't expect marvolous things. However I'm making it from the bottom up. No cheating, only my code and some frameworks maybe.. (right now only bootstrap) :) <br>If you are an employer, please take a look at my resume.";
 }
 
 // var btn = document.createElement("BUTTON");        // Create a <button> element
 // var t = document.createTextNode("CLICK ME");       // Create a text node
 // btn.appendChild(t);                                // Append the text to <button>
 // document.body.appendChild(btn);                    // Append <button> to <body> 
-
-function displayResumePage(){
-    clearPage();
-    heading.innerHTML = "Jonas Ladefoged Holm";
-    ptag.innerHTML = "uei";
-    for(var i = 0; i < 10; i++){
-    var tag = document.createElement("P");
-        document.getElementById("content").appendChild(tag);
-        tag.innerHTML = "HEJ " + i;
-    }
-}
 
 function clearPage(){
      var parent = document.getElementById("content");
@@ -48,18 +30,47 @@ function clearPage(){
      }
 }
 
+function displayResumePage(){
+    clearPage();
+    heading.innerHTML = "Resume";
+    ptag.innerHTML = "My resume would go here, here is a pdf version until then.";
+    
+    var pdflink = document.createElement("a");
+    document.getElementById("content").appendChild(pdflink);
+    pdflink.innerHTML = "<span class=\"glyphicon glyphicon-cloud-download\"></span> resume.pdf";
+    pdflink.href = "resume.pdf";
 
+}
+
+function displayBlog(){
+    clearPage();
+    heading.innerHTML = "Blog";
+    ptag.innerHTML = "Right now I'm just testing a loop";
+    for(var i = 0; i < 10; i++){
+        var tag = document.createElement("P");
+        document.getElementById("content").appendChild(tag);
+        tag.innerHTML = "This is a loop itteration: " + i;
+    }
+}
 
 function displayProjectsPage(){
     clearPage();
-    heading.innerHTML = "Masser af projecter";
-    ptag.innerHTML = "Bla noget med en app";
+    heading.innerHTML = "Lot's </br> of cool projects"
+    ptag.innerHTML = "Projects goes here";
 }
 
 function displayDreamFirmPage(){
     clearPage();
-    heading.innerHTML = "Dreaming about a firm";
-    ptag.innerHTML = "Det bedste firm in the wooooorld!";
+    heading.innerHTML = "Dreaming about a company";
+    ptag.innerHTML = "Will it be you?? (more to come)";
+}
+
+function underConstruction(){
+    var tag = document.createElement("h2");
+    tag.id = "warning";
+    var div = document.body;
+    div.insertBefore(tag, div.childNodes[0]);
+    tag.innerHTML = "<span class=\"glyphicon glyphicon-wrench\"></span>  Site under construction.  <span class=\"glyphicon glyphicon-wrench\"></span>";
 }
 
 
