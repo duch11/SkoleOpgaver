d5dd5765218485234d9996294830c4ca06ce1750
Jonas Ladefoged Holm <jonas@holm.tech>
2017-06-18 03:29:07 +0200

    hej

--- BookCatalogLibrary/Head.java
+++
@@ -1,8 +0,0 @@
-public class Head {
-    public static void main(String[] args) {
-        UserInterface ui = new UserInterface();
-        Catalog catalog = new Catalog();
-        Library lib = new Library(catalog);
-        ui.addBooks(lib);
-    }
-}
--- Inheritance/Arv.java
+++
@@ -1,8 +0,0 @@
-import film.*;
-public class Arv {
-    public static void main(String[] args) {
-        System.out.println("Tester nu arv.");
-        Cd cd1 = new Cd("",2,"");
-        cd1.play();
-    }
-}
